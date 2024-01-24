2bd36db3bc5269a8daa600cbde0641414af4a666
Javi MN <j4v13r15@hotmail.com>
2024-01-24 17:14:34 -0500

    Terminated paso 3

---
+++ Part1/README.md
@@ -0,0 +1,18 @@
+# Ejercicios de la Parte 1
+
+## 1.1
+1.1: información del curso, paso 1
+Refactoriza el código para que conste de tres componentes nuevos: Header, Content y Total. 
+
+## 1.2
+información del curso, paso 2
+Refactoriza el componente Content 
+
+## 1.3
+Descripción del ejercicio 1.3...
+
+## 1.4
+Descripción del ejercicio 1.4...
+
+## 1.5
+Descripción del ejercicio 1.5...
--- Part1/courseinfo/src/App.jsx
+++ Part1/courseinfo/src/App.jsx
@@ -1,4 +1,5 @@
 const Part1 = (props) => {
+  console.log('props part1', props);
   return (
     <div>
       <p>
@@ -9,6 +10,7 @@ const Part1 = (props) => {
 }
 
 const Part2 = (props) => {
+  console.log('props part2', props);
   return (
     <div>
       <p>
@@ -19,6 +21,7 @@ const Part2 = (props) => {
 }
 
 const Part3 = (props) => {
+  console.log('props part3', props);
   return (
     <div>
       <p>
@@ -57,18 +60,23 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
   return (
     <>
       <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+      <Content part1={part1.name} part2={part2.name} part3={part3.name} exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises}/>
+      <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises}/>
     </>
   )
 }
